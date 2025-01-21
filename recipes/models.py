b7fc2aaa6a4cbe24ab99769a04621248a2acb522
Cass Beckford <CassJBeckford282@outlook.com>
2025-01-21 14:44:44 +0000

    add name, description, difficulty and time to Recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -5,11 +5,15 @@ class Category(db.Model):
     category_title = db.Column(db.String(25), unique=True, nullable=False)
 
     def __repr__(self):
-        return self 
+        return self.category_title
 
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    recipe_name = db.Column(db.String(50), unique=True, nullable=False)
+    recipe_description = db.Column(db.Text, nullable=False)
+    recipe_difficulty = db.Column(db.Integer(10), nullable=False)
+    recipe_time = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return self 
