30acf5f474700fbd5a15d9e68de7214aac2dde76
Cass Beckford <CassJBeckford282@outlook.com>
2025-01-21 15:21:53 +0000

    return string for Recipe class including id, name and difficulty

--- recipes/models.py
+++ recipes/models.py
@@ -3,6 +3,7 @@ from recipes import db
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     category_title = db.Column(db.String(25), unique=True, nullable=False)
+    recipes = db.relationship("Recipe", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         return self.category_title
@@ -17,4 +18,6 @@ class Recipe(db.Model):
     category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
-        return self 
+        return "#{0} - Recipe: {1} | Difficulty: {2}".format(
+            self.id, self.recipe_name, self.recipe_difficulty
+        ) 
