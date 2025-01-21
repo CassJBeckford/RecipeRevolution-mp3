be2a8187c3b2d65122b3b166537a21932932c006
Cass Beckford <CassJBeckford282@outlook.com>
2025-01-21 14:11:35 +0000

    add __repr__ function that takes itself as an argument

--- recipes/models.py
+++ recipes/models.py
@@ -3,6 +3,12 @@ from recipes import db
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
+    def __repr__(self):
+        return self 
+
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+
+    def __repr__(self):
+        return self 
