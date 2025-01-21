b33b823285dfe6c5197599bc719ab4a26104923b
Cass Beckford <CassJBeckford282@outlook.com>
2025-01-21 14:18:38 +0000

    add category_title and return self.category_title in function

--- recipes/models.py
+++ recipes/models.py
@@ -2,6 +2,7 @@ from recipes import db
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    category_title = db.Column(db.String(25), unique=True, nullable=False)
 
     def __repr__(self):
         return self 
