11b81fab288a3827cc3f95439a4097046871c5ad
Cass Beckford <CassJBeckford282@outlook.com>
2025-01-22 13:35:45 +0000

    add route and view function for add_categories, using methods GET and POST since it will be submitting to the database

--- recipes/routes.py
+++ recipes/routes.py
@@ -3,9 +3,6 @@ from recipes import app, db
 from recipes.models import Category, Recipe
 
 
-
-
-
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -14,6 +11,11 @@ def home():
 @app.route("/categories")
 def categories():
     return render_template("categories.html")
+
+
+@app.route("/add_categories", methods=["GET", "POST"])
+def add_categories():
+    return render_template("add_categories.html")
 
 
 @app.route("/recipe")
