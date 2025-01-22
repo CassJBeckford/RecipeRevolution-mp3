579e1678f20a9ab7bba5a602bfb28853fe33c4ad
Cass Beckford <CassJBeckford282@outlook.com>
2025-01-22 10:54:28 +0000

    initialise navbar functionality

---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Materialize sidenav initialisation (https://materializecss.com/navbar.html)
+    let navbar = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(navbar);
+});
