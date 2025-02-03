38e2b2afd2e82f302077c91b6ec80ee5533724d7
Cass Beckford <CassJBeckford282@outlook.com>
2025-02-03 17:57:21 +0000

    pyenv shell 3.12.2

--- cookbooks/models.py
+++ cookbooks/models.py
@@ -1,5 +1,14 @@
 from cookbooks import db
 
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(260), nullable=False)
+    categories = db.relationship('Category', backref='Users', lazy=True)
+
+    def __repr__(self):
+        return self.user_name
+
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     category_title = db.Column(db.String(25), unique=True, nullable=False)
@@ -25,12 +34,3 @@ class Recipe(db.Model):
         return "#{0} - Recipe: {1} | Difficulty: {2}".format(
             self.id, self.recipe_name, self.recipe_amount
         ) 
-
-
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(260), nullable=False)
-
-    def __repr__(self):
-        return self.user_name
