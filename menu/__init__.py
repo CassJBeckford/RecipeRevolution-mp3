4f8b8f6e84b4372068f679fe52d7ee432c3ac890
cassjbeckford <cassjbeckford282@outlook.com>
2025-03-04 16:46:55 +0000

    add routes for werkzeug security

--- menu/__init__.py
+++ menu/__init__.py
@@ -1,8 +1,8 @@
 import os
-from flask import Flask 
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env  
+    import env
 
 app = Flask(__name__)
 
