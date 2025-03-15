53ea0c54572f4e44af39c93602fc13545d8a0a2e
cassjbeckford <cassjbeckford282@outlook.com>
2025-03-15 18:33:43 +0000

    add comments

--- menu/__init__.py
+++ menu/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env
+    import env  # noqa
 
 app = Flask(__name__)
 
@@ -13,9 +13,9 @@ if os.environ.get("DEVELOPMENT") == "True":
 else:
     uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
-         uri = uri.replace("postgres://", "postgresql://", 1)
+        uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 db = SQLAlchemy(app)
 
-from menu import routes
+from menu import routes # noqa
